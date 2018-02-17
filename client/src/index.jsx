09b2bc3f9d3b3b82c365295781ce3a18d8f9c132
Hack Reactor Student <student@hackreactor.com>
2018-02-17 16:22:12 -0500

    final commit prior to pres

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -12,18 +12,18 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      username: 'chris',
+      username: '',
       balance: 0,
       coin: '',
       chart: {
         balances: [
-          { key: 'BTC', value: 100 }
+          { key: '', value: 0 }
         ],
         chartSize: 400,
         chartPadding: 200,
         chartLabels: true
       },
-      cryptoData: [{}]
+      tooltip: ''
     }
     this.addCoinBalance = this.addCoinBalance.bind(this);
     this.updateCharts = this.updateCharts.bind(this);
@@ -31,50 +31,12 @@ class App extends React.Component {
     this.updateCoinState = this.updateCoinState.bind(this);
     this.getCurrentExchangeRates = this.getCurrentExchangeRates.bind(this);
     this.calculateUSDbalances = this.calculateUSDbalances.bind(this);
+    this.chartMouseMoveHandler = this.chartMouseMoveHandler.bind(this);
+    this.chartMouseMoveHandler = this.chartMouseMoveHandler.bind(this);
+    this.chartMouseOutHandler = this.chartMouseOutHandler.bind(this);
+    this.chartCreateTooltip = this.chartCreateTooltip.bind(this);
   }
 
-  componentDidMount() {
-    this.updateCharts();
-  }
-
-  calculateUSDbalances(rates) {
-    console.log('this is the state of balances: ', this.state.chart.balances)
-    let userAccountUpdate = {
-        balances: [],
-        chartSize: this.state.chart.chartSize,
-        chartPadding: this.state.chart.chartPadding,
-        chartLabels: this.state.chart.chartLabels
-    };
-    this.state.chart.balances.forEach((wallet) => {
-      userAccountUpdate.balances.push({
-        value: wallet.value * rates[wallet.key],
-        key: wallet.key
-      })
-    })
-    console.log('setting the chart state with: ', userAccountUpdate)
-    this.setState({
-      chart: userAccountUpdate
-    })
-  }
-
-  getCurrentExchangeRates () {
-    axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
-         .then((response) => {
-           console.log('this is the API response.data[0]: ', response.data[0].symbol);
-           let cryptoData = response.data;
-           console.log('this is crypto data: ', cryptoData);
-           let rates = {};
-           cryptoData.forEach((crypto) => {
-             rates[crypto.symbol] = crypto.price_usd;
-           })
-           console.log('this is the rates object: ', rates);
-           this.calculateUSDbalances(rates);
-         })
-         .catch((error) => {
-           console.log('ERROR in getCurrentExchangeRates, error: ', error);
-         })
-  }
-  
   addCoinBalance() {
     axios.post('/submit', {
       username: this.state.username,
@@ -112,13 +74,77 @@ class App extends React.Component {
       })
       console.log('setting the chart state with: ', userAccountUpdate)
       this.setState({
-        chart: userAccountUpdate
+        chart: userAccountUpdate,
+        balance: '',
+        coin: ''
       })
       this.getCurrentExchangeRates();
     })
     .catch((error) => {
       console.log('ERROR in updateCharts, error: ', error);
     })
+  }
+
+  getCurrentExchangeRates () {
+    axios.get('https://api.coinmarketcap.com/v1/ticker/?limit=100')
+         .then((response) => {
+           let cryptoData = response.data;
+           let rates = {};
+           cryptoData.forEach((crypto) => {
+             rates[crypto.symbol] = crypto.price_usd;
+           })
+           this.calculateUSDbalances(rates);
+         })
+         .catch((error) => {
+           console.log('ERROR in getCurrentExchangeRates, error: ', error);
+         })
+  }
+
+  calculateUSDbalances(rates) {
+    let userAccountUpdate = {
+        balances: [],
+        chartSize: this.state.chart.chartSize,
+        chartPadding: this.state.chart.chartPadding,
+        chartLabels: this.state.chart.chartLabels
+    };
+    this.state.chart.balances.forEach((wallet) => {
+      userAccountUpdate.balances.push({
+        value: wallet.value * rates[wallet.key],
+        key: wallet.key
+      })
+    })
+    this.setState({
+      chart: userAccountUpdate
+    })
+  }
+
+  chartMouseOverHandler(d, e) {
+    let bal = Math.floor(d.data.value);
+    console.log(`You have ${d.data.key} balance is $${bal} USD`);
+  }
+
+  chartMouseMoveHandler(e) {
+    // if (this.state.showToolTip) {
+    //   this.setState({top: e.y, left: e.x});
+    // }
+  }
+
+  chartMouseOutHandler() {
+    // this.setState({showToolTip: false});
+  }
+
+  chartCreateTooltip() {
+    /*if (this.state.showToolTip) {
+      return (
+        <ToolTip
+          top={this.state.top}
+          left={this.state.left}
+        >
+          The value of {'bitocin'} is {'something'}
+        </ToolTip>
+      );
+    }
+    return false;*/
   }
 
   updateBalanceState(balance) {
@@ -165,7 +191,11 @@ class App extends React.Component {
         />
         <CryptoChart 
           chartData={this.state.chart}
+          mouseOverHandler={this.chartMouseOverHandler}
+          mouseOutHandler={this.chartMouseOutHandler}
+          mouseMoveHandler={this.chartMouseMoveHandler}
         />
+        <h5></h5>
       </div>
     )
   }
